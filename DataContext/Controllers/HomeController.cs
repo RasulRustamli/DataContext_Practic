cd07675170e482c7429ae9c713e787e76ca1a5c6
RasulRustamli <rasular@code.edu.az>
2022-01-05 22:49:46 +0400

    Add project files.

---
+++ DataContext/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using DataContext.DAL;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataContext.Controllers
+{
+    public class HomeController:Controller
+    {
+        private readonly Context _context;
+        public HomeController(Context context)
+        {
+            _context = context;    
+        }
+
+        public IActionResult Index()
+        {
+
+            var stuList = _context.Students.ToList();
+            //_context.Students.Add();
+            //_context.Students.Remove();
+            //_context.Students.Update();
+
+            return View(stuList);
+        }
+    }
+   
+}
---
+++ DataContext/DAL/Context.cs
@@ -0,0 +1,18 @@
+﻿using DataContext.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataContext.DAL
+{
+    public class Context:DbContext
+    {
+        public Context(DbContextOptions<Context>options):base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
